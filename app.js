b63a216c69f8f6bf544c16561aa13a2a9820c5d7
MartiPuigV <119410972+MartiPuigV@users.noreply.github.com>
2025-03-01 21:31:07 +0100

    complete refactor for 2 in 1. test n.1

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import path from 'path'
-import { addDate, retrieveUploads } from './methods.js'
+import { fileURLToPath } from 'url'
+import { addDate, retrieveUploads, getLastDays } from './methods.js'
 
 dotenv.config()
 
 const app = express()
+const router = express.Router()
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', cors(), (_, res) => {
     res.status(200).send('Alive')
@@ -54,16 +58,35 @@ app.get('/muons/:quantity', cors(), async (req, res) => {
     try {
         const lines = await retrieveUploads(quantity);
         res.status(200).send(lines);
-    } catch (error) {
+    } catch(error) {
         console.error(error);
         res.status(500).send('Error reading file');
     }
 })
 
-app.use((err, _, res, _) => {
+app.get('/days/:days', cors(), async (req, res) => {
+    // Retrieve last coincidences during the last 'n' days
+    const days = req.params.days;
+    try {
+        const lines = await getLastDays(days);
+        res.status(200).send(lines);
+    } catch(error) {
+        console.error(error);
+        res.status(500).send('Error reading file');
+    }
+
+})
+
+router.get('/dashboard', cors(), function(req, res) {
+    res.sendFile(path.resolve('index.html'));
+})
+
+app.use((err, req, res, _) => {
     console.error(err.stack)
     res.status(500).send('Something broke!')
 })
+
+app.use('/', cors(), router);
 
 app.listen(process.env.PORT, () => {
     console.log('Application deployed with utmost success')
--- methods.js
+++ methods.js
@@ -14,7 +14,9 @@ export async function retrieveUploads(amount) {
     try {
         const data = await fs.readFile('log.txt', 'utf-8');
         let lines = data.split('\n');
-        lines = lines.slice(-amount-1, -1);
+        if (amount != 0) {
+            lines = lines.slice(-amount-1, -1);
+        }
         lines.reverse();
         lines = removeCarriageReturn(lines);
         return lines;
@@ -22,4 +24,22 @@ export async function retrieveUploads(amount) {
         console.error('Error reading file:', error);
         throw error;
     }
+}
+
+export async function getLastDays(days) {
+    const dates = await retrieveUploads(0);
+    console.log(dates);
+    const today = dates[0];
+    console.log(today);
+    const limit = today-(days*24*3600); // Transform days to seconds
+    console.log(limit);
+    let result = [];
+    dates.forEach(day => {
+        if (day > limit) {
+            result.push(day);
+        } else {
+            return
+        }
+    });
+    return result;
 }
